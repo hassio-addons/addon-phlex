4a9cdd344e0afd53d3c191db284bffc5ca353001
ludeeus <joasoe@gmail.com>
2019-03-24 13:58:20 +0100

    🔨 Rewrite to use bashio

--- phlex/rootfs/etc/cont-init.d/20-phlex.sh
+++ phlex/rootfs/etc/cont-init.d/20-phlex.sh
@@ -1,12 +1,10 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: Phlex
 # Configures Phlex
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
 
-if ! hass.file_exists '/data/config.php'; then
+if ! bashio::fs.file_exists '/data/config.php'; then
     cp /opt/default_config.php /data/config.php
 fi
 
--- phlex/rootfs/etc/cont-init.d/30-nginx.sh
+++ phlex/rootfs/etc/cont-init.d/30-nginx.sh
@@ -1,28 +1,26 @@
-#!/usr/bin/with-contenv bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
 # Community Hass.io Add-ons: Phlex
 # Configures NGINX for use with Phlex
 # ==============================================================================
-# shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
 
 declare certfile
 declare keyfile
 
 # Enable SSL
-if hass.config.true 'ssl'; then
+if bashio::config.true 'ssl'; then
     rm /etc/nginx/nginx.conf
     mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
 
-    certfile=$(hass.config.get 'certfile')
-    keyfile=$(hass.config.get 'keyfile')
+    certfile=$(bashio::.config 'certfile')
+    keyfile=$(bashio::.config 'keyfile')
 
     sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
     sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
 fi
 
 # Disables IPv6 in case its disabled by the user
-if ! hass.config.true 'ipv6'; then
+if ! bashio::config.true 'ipv6'; then
     sed -i '/listen \[::\].*/ d' /etc/nginx/nginx.conf
 fi
 
