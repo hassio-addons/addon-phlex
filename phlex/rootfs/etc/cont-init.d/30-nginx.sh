7741005e91d83d604d63ec356dcb5f1be2059a76
ludeeus <joasoe@gmail.com>
2019-03-24 14:02:15 +0100

    🚑 Fix typo

--- phlex/rootfs/etc/cont-init.d/30-nginx.sh
+++ phlex/rootfs/etc/cont-init.d/30-nginx.sh
@@ -12,8 +12,8 @@ if bashio::config.true 'ssl'; then
     rm /etc/nginx/nginx.conf
     mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
 
-    certfile=$(bashio::.config 'certfile')
-    keyfile=$(bashio::.config 'keyfile')
+    certfile=$(bashio::config 'certfile')
+    keyfile=$(bashio::config 'keyfile')
 
     sed -i "s/%%certfile%%/${certfile}/g" /etc/nginx/nginx.conf
     sed -i "s/%%keyfile%%/${keyfile}/g" /etc/nginx/nginx.conf
