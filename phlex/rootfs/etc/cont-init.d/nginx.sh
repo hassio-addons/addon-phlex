7588a58d88545397cebcab7dfd55694b6cb6b593
Franck Nijhof <frenck@addons.community>
2019-04-21 09:02:35 +0200

    :hammer: Refactor of init scripts

--- phlex/rootfs/etc/cont-init.d/nginx.sh
+++ phlex/rootfs/etc/cont-init.d/nginx.sh
@@ -3,9 +3,10 @@
 # Community Hass.io Add-ons: Phlex
 # Configures NGINX for use with Phlex
 # ==============================================================================
-
 declare certfile
 declare keyfile
+
+bashio::config.require.ssl
 
 # Enable SSL
 if bashio::config.true 'ssl'; then
